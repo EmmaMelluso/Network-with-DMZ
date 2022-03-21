bdd2396b1bbc22a4383ae95fda908c651b3a9523
EmMe <emmamelluso@libero.it>
2022-03-21 15:26:17 +0100

    aggiornamento

--- Progetto/config.sh
+++ Progetto/config.sh
@@ -13,70 +13,65 @@ cd ftpimmage/
 ./build.sh
 cd ..
 
-#Configurazione rete aziendale
+						#Configurazione rete aziendale
 
 ##############################################################################################################################
-#creo la sottorete "rete_esterna" e gli connetto un host							             #
+#				Creo la sottorete "rete_esterna" e gli connetto un host					     #
 ##############################################################################################################################
 docker network create --driver bridge --subnet=192.1.1.0/24 rete_esterna
 docker run --privileged --network=rete_esterna --ip 192.1.1.2 -td --name=cliente1 hostubuntu bash
 
 ##############################################################################################################################
-#creo la sottorete "dmz" e gli connetto un webserver							                     #
+#				Creo la sottorete "dmz" e gli connetto un webserver					     #
 ##############################################################################################################################
 docker network create --driver bridge --subnet=192.1.2.0/24 dmz
 docker run --privileged --network=dmz --ip 192.1.2.2 -p80:80 -p443:443 -tdi --name=webserver linode/lamp bash
 docker exec --privileged -t webserver service apache2 start
 
 ##############################################################################################################################
-#connetto FTP server a sottorete "dmz"						                     			     #
+#				Connetto FTP server a sottorete "dmz"						             #
 ##############################################################################################################################
 docker run --privileged --network=dmz --ip 192.1.2.4 -p20:20 -p21:21 -tdi --name=ftpserver ftpser bash
 
 ##############################################################################################################################
-#connetto SMTP server a sottorete "dmz"						                     			     #
-##############################################################################################################################
-#docker run --privileged --network=dmz --ip 192.1.2.4 -p25:25 -tdi --name=smtpserver nsunina/postfix:v1.1 bash
-
-##############################################################################################################################
-#connetto DNS server a sottorete "dmz"						                     			     #
+#				Connetto DNS server a sottorete "dmz"						             #
 ##############################################################################################################################
 docker run --privileged --network=dmz --ip 192.1.2.3 -p53:53/udp -tdi --name=dnsser cosmicq/docker-bind:latest bash
 
 ##############################################################################################################################
-#creo la sottorete "rete_interna" e gli connetto un host							             #
+#				Creo la sottorete "rete_interna" e gli connetto un host				             #
 ##############################################################################################################################
 docker network create --driver bridge --subnet=192.1.3.0/24 rete_interna
 docker run --privileged --network=rete_interna --ip 192.1.3.2 -td --name=host1 hostubuntu bash
 
 ##############################################################################################################################
-#creo la sottorete "rete_intermedia"						                   			     #
+#				Creo la sottorete "rete_intermedia"						             #
 ##############################################################################################################################
 docker network create --driver bridge --subnet=192.1.4.0/24 rete_intermedia
 
 ##############################################################################################################################
-#run immagine firewall esterno e connessione a rete esterna						                     #
+#				Run immagine firewall esterno e connessione a rete esterna				     #
 ##############################################################################################################################
 docker run --privileged --sysctl net.ipv4.ip_forward=1 --network=rete_esterna --ip 192.1.1.5 -td --name=firewall1 customfirewall bash
 
 ##############################################################################################################################
-#run immagine firewall interno e connessione a rete interna						                     #
+#				Run immagine firewall interno e connessione a rete interna				     #
 ##############################################################################################################################
 docker run --privileged --sysctl net.ipv4.ip_forward=1 --network=rete_interna --ip 192.1.3.6 -td --name=firewall2 customfirewall bash
 
 ##############################################################################################################################
-#connetto firewall esterno a rete dmz						                                             #
+#				Connetto firewall esterno a rete dmz						             #
 ##############################################################################################################################
 docker network connect --ip 192.1.2.5 dmz firewall1
 
 ##############################################################################################################################
-#connetto firewall esterno e interno a rete intermedia						                             #
+#				Connetto firewall esterno e interno a rete intermedia					     #
 ##############################################################################################################################
 docker network connect --ip 192.1.4.5 rete_intermedia firewall1
 docker network connect --ip 192.1.4.6 rete_intermedia firewall2
 
 ##############################################################################################################################
-#Assegno i gateway di default					                            				     #
+#				Assegno i gateway di default					                            				     #
 ##############################################################################################################################
 
 docker exec firewall1 route add default gw firewall2
@@ -95,66 +90,88 @@ docker network inspect dmz
 docker network inspect rete_intermedia
 docker ps 
 
-#SETTO IPTABLES
-
-# Cancellazione delle regole presenti nelle chains
+							#SETTO IPTABLES
+##############################################################################################################################
+#				Cancellazione delle regole presenti nelle chains		                             #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -F
 docker exec --privileged -t firewall2 iptables -F
 docker exec --privileged -t firewall1 iptables -F -t nat
 docker exec --privileged -t firewall2 iptables -F -t nat
 
-# Eliminazione delle chains non standard vuote
+##############################################################################################################################
+#				Eliminazione delle chains non standard vuote			                             #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -X
 docker exec --privileged -t firewall2 iptables -X
 
-# Policy di base per firewall1(blocco tutto quello che non è esplicitamente consentito)
+##############################################################################################################################
+#		Policy di base per firewall1 e firewall2 (blocco tutto quello che non è esplicitamente consentito)           #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -P INPUT DROP
 docker exec --privileged -t firewall1 iptables -P OUTPUT DROP
 docker exec --privileged -t firewall1 iptables -P FORWARD DROP
 
-# Policy di base per firewall2(blocco tutto quello che non è esplicitamente consentito)
 docker exec --privileged -t firewall2 iptables -P INPUT DROP
 docker exec --privileged -t firewall2 iptables -P OUTPUT DROP
 docker exec --privileged -t firewall2 iptables -P FORWARD DROP
 
-#Firewall Esterno (firewall1)
-
-# Protezione da syn flood
-# Vedere il rate medio per protezione adeguata
+##############################################################################################################################
+#					Protezione da syn flood							             #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
 docker exec --privileged -t firewall2 iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
 
-# Protezione da ping of death
-# Anche qui vedi rate medio
+##############################################################################################################################
+#					Protezione da ping of death						             #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -A FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
 docker exec --privileged -t firewall2 iptables -A FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
 
-#1-Inoltra tutti i pacchetti provenienti dall'esterno sull'interfaccia della DMZ
+
+						#Firewall Esterno (firewall1)
+##############################################################################################################################
+#	1-Inoltra tutti i pacchetti provenienti dall'esterno (eth0) sull'interfaccia della DMZ (eth1)	                     #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -t filter -A FORWARD -i eth0 -o eth1 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 docker exec --privileged -t firewall1 iptables -t filter -A FORWARD -i eth1 -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-#2-Inoltro tutti i pacchetti provenienti dall'interno eth2 a eth1 
+##############################################################################################################################
+#	2-Inoltro tutti i pacchetti provenienti dall'interno (eth2) sull'interfaccia della DMZ (eth1) 	                     #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -t filter -A FORWARD -i eth2 -o eth1 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 docker exec --privileged -t firewall1 iptables -t filter -A FORWARD -i eth1 -o eth2 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-#Firewall Interno (firewall2)
-#1-Inoltro Pacchetti icmp da rete interna a rete intermedia
+
+						#Firewall Interno (firewall2)
+##############################################################################################################################
+#	1-Inoltro Pacchetti icmp da rete interna (eth0) a rete intermedia (eth1)				 	     #
+##############################################################################################################################
 #Un host interno non puo' raggiungere un cliente che fa parte della rete esterna
 docker exec --privileged -t firewall2 iptables -t filter -A FORWARD -p icmp -i eth0 -o eth1 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 docker exec --privileged -t firewall2 iptables -t filter -A FORWARD -p icmp -i eth1 -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Droppo tutti i tentativi di connessione provenienti dalla DMZ verso la rete interna (non matcha è solo un controllo ulteriore)
 docker exec --privileged -t firewall2 iptables -A FORWARD -p tcp -s 192.1.2.0/24 --syn -j DROP
 
-#Natting da qualsiasi host della rete esterna dmz
-#1-Web Server
+
+					#Natting da qualsiasi host della rete esterna dmz
+##############################################################################################################################
+#							1-Web Server				                  	     #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-dest 192.1.2.2
 docker exec --privileged -t firewall1 iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 443 -j DNAT --to-dest 192.1.2.2
 
-#2-DNS Server
+##############################################################################################################################
+#							2-DNS Server				                  	     #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -t nat -A PREROUTING -p udp -i eth0 --dport 53 -j DNAT --to-dest 192.1.2.3
 
-#3-FTP Server
+##############################################################################################################################
+#							3-FTP Server				                  	     #
+##############################################################################################################################
 docker exec --privileged -t firewall1 iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 21 -j DNAT --to-dest 192.1.2.4
+
+
 
 # Elimino pacchetti non validi
 docker exec --privileged -t firewall1 iptables -A INPUT -m state --state INVALID -j DROP
